8cdb852ce9679e70278b68143cf5a271787923d4
aaron <969340992@qq.com>
2020-03-31 09:35:44 +0800

    add polly test

--- DotNetCoreWebApiLearning/Startup.cs
+++ DotNetCoreWebApiLearning/Startup.cs
@@ -17,6 +17,8 @@ using DotNetCoreWebApiLearning.Services;
 using DotNetCoreWebApiLearning.Data;
 using System.Reflection;
 using System.IO;
+using Polly;
+using System.Net.Http;
 
 namespace DotNetCoreWebApiLearning
 {
@@ -59,6 +61,54 @@ namespace DotNetCoreWebApiLearning
                 // 启用xml注释. 该方法第二个参数启用控制器的注释，默认为false.
                 c.IncludeXmlComments(xmlPath, true);
             });
+
+            #region polly
+
+            var fallbackResponse = new HttpResponseMessage();
+            fallbackResponse.Content = new StringContent("fallback");
+            fallbackResponse.StatusCode = System.Net.HttpStatusCode.TooManyRequests;
+
+            //重试
+            services.AddHttpClient("github", c =>
+            {
+                //基址
+                c.BaseAddress = new System.Uri("https://api.github.com/");
+                // Github API versioning
+                c.DefaultRequestHeaders.Add("Accept", "application/vnd.github.v3+json");
+                // Github requires a user-agent
+                c.DefaultRequestHeaders.Add("User-Agent", "HttpClientFactory-Sample");
+                //AddTransientHttpErrorPolicy主要是处理Http请求的错误，如HTTP 5XX 的状态码，HTTP 408 的状态码 以及System.Net.Http.HttpRequestException异常
+            }).AddTransientHttpErrorPolicy(p =>
+                //WaitAndRetryAsync参数的意思是：每次重试时等待的睡眠持续时间。
+                p.WaitAndRetryAsync(3, _ => TimeSpan.FromMilliseconds(600))
+            );
+           
+            services.AddHttpClient("test", c =>
+            {
+                //基址
+                c.BaseAddress = new System.Uri("http://localhost:5000/");
+                // Github API versioning
+                c.DefaultRequestHeaders.Add("Accept", "application/vnd.github.v3+json");
+                // Github requires a user-agent
+                c.DefaultRequestHeaders.Add("User-Agent", "HttpClientFactory-Sample");
+            })
+            //降级
+            .AddPolicyHandler(Policy<HttpResponseMessage>.Handle<Exception>().FallbackAsync(fallbackResponse, async b =>
+            {
+                Console.WriteLine($"fallback here {b.Exception.Message}");
+            }))
+            //熔断
+            .AddPolicyHandler(Policy<HttpResponseMessage>.Handle<Exception>().CircuitBreakerAsync(2, TimeSpan.FromSeconds(4), (ex, ts) =>
+            {
+                Console.WriteLine($"break here {ts.TotalMilliseconds}");
+            }, () =>
+            {
+                Console.WriteLine($"reset here ");
+            }))
+            //超时
+            .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
+
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
